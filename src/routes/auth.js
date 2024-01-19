32b00df9f9d715e3f62e90e2e82470d130ada6a8
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-01-19 10:39:06 +0100

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,33 +1,30 @@
 // routes/auth.route.js
-import express from 'express';
-import authController from '../controllers/auth.controller.js';
-import { jwtMiddleware } from '../middleware/jwtMiddleware.js';
-import  adminMiddleware  from '../middleware/adminMiddleware.js';
-
+import express from "express";
+import authController from "../controllers/auth.controller.js";
+import { jwtMiddleware } from "../middleware/jwtMiddleware.js";
+import adminMiddleware from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
 // Route de création de compte
-router.post('/register', authController.registerUser);
+router.post("/register", authController.registerUser);
 //Route de connexion
-router.post('/login', authController.loginUser);
+router.post("/login", authController.loginUser);
 //Route deconnéxion
-router.post('/logout', jwtMiddleware, authController.logoutUser);
+router.post("/logout", jwtMiddleware, authController.logoutUser);
 // Route suppréssion de compte
-router.delete('/delete-account', jwtMiddleware, authController.deleteUser);
+router.delete("/delete-account", jwtMiddleware, authController.deleteUser);
 // Route de mise à jour des informations du compte
-router.put('/update-account', jwtMiddleware, authController.updateUser);
+router.put("/update-account", jwtMiddleware, authController.updateUser);
 // Route pour mettre à jour le mot de passe
-router.put('/update-password', jwtMiddleware, authController.updatePassword);
+router.put("/update-password", jwtMiddleware, authController.updatePassword);
 
+router.get("/dashboard", jwtMiddleware, adminMiddleware, (req, res) => {
+  res.json({ message: "Coucou, je suis admin !" });
+});
 
-router.get('/dashboard', jwtMiddleware, adminMiddleware, (req, res) => {
-    res.json({ message: 'Coucou, je suis admin !' });
-  });
-  
-  router.get('/test',(req, res) => {
-    res.json({ message: 'Coucou, je suis un test!' });
-  });
-  
+router.get("/test", (req, res) => {
+  res.json({ message: "Coucou, je suis un test!" });
+});
 
 export default router;
